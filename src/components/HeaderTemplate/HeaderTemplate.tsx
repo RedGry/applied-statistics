6298239de564d599437830d8a9900e9ab873bd74
Egor <trottimus.2001@mail.ru>
2020-12-08 21:16:35 +0300

    Добавил упражнения из 1 недели (Только 2 :( )

--- src/components/HeaderTemplate/HeaderTemplate.tsx
+++ src/components/HeaderTemplate/HeaderTemplate.tsx
@@ -15,17 +15,9 @@ export abstract class HeaderTemplate extends React.Component {
                     <br />
 
                     Автор:{' '}
-                    <a href="https://byprogminer.ru/vk" target="_blank" rel="noopener noreferrer">Доморацкий Эридан</a>.{' '}
+                    <a href="https://vk.com/egoredgry" target="_blank" rel="noopener noreferrer">RedGry</a>.{' '}
                     <strong>В случае ошибки (например, вывода неправильного ответа), пишите, пожалуйста, в ЛС!</strong>
                     <br />
-
-                    <small>
-                        (донатить сюда: <strong>5321<span style={{ paddingLeft: '0.5ch' }} />{' '}
-                        8687<span style={{ paddingLeft: '0.5ch' }} />2723<span style={{ paddingLeft: '0.5ch' }} />{' '}
-                        3422</strong> — MasterCard Банк СПб;{' '}
-
-                        <a href="https://my.qiwi.com/Erydan-DMVuYwfjtW" target="_blank" rel="noopener noreferrer">Qiwi</a>)
-                    </small>
                 </Panel>
             </div>
         );
